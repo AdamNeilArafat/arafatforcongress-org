4d0f172e196c5c9671d52b6093755f906c705c30
AdamNeilArafat <adam.n.arafat@gmail.com>
2025-08-31 12:10:20 -0700

    Revert "Standardize header/footer and unify issues page"

--- assets/site.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  const path = location.pathname.replace(/\/+$/,'');
-  document.querySelectorAll('.nav a').forEach(a => {
-    const href = new URL(a.getAttribute('href'), location.origin).pathname.replace(/\/+$/,'');
-    if (href === path) { a.classList.add('active'); a.setAttribute('aria-current','page'); }
-  });
-})();
